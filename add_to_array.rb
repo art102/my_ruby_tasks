59e00041872cd237ea9476684da9999cc0c5356d
Artur Daukaev <mckorven@gmail.com>
2017-06-10 19:52:30 +0500

    Add comments

--- add_to_array.rb
+++ add_to_array.rb
@@ -6,7 +6,7 @@ while 2 + 2 == 4
   print "Кого добавить в список?: "
   user = gets.strip.capitalize
    
-  if user == ""
+  if user == "" #If enter 'Enter' else print list & exit
     names.each_with_index do |names, index|
       puts "#{index + 1}. #{names}"
     end
