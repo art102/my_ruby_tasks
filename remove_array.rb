c34bb0a707730bbddbdeb718c7fdd5b24d4f38b1
Artur Daukaev <mckorven@gmail.com>
2017-06-10 19:07:29 +0500

    Optimization prog, delet 'if..elsif'

--- remove_array.rb
+++ remove_array.rb
@@ -1,32 +1,14 @@
-@students = %w[harry ron hermiona nevil polumna malfoy]
+students = %w[harry ron hermiona nevil polumna malfoy]
 
-def print_name
-  @students.each_with_index do |name, index|
-    puts "#{index + 1}.#{name}"
+while students.size > 0
+  students.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
   end
-end
-
-print_name
-
-while @students.size > 0
+  
   print 'Who you want to kill?: '
   remove = gets.strip.to_i
-  remove = remove - 1
+  remove = remove - 1 
+   
+  students.delete_at remove 
   
-  if remove == 0
-    @students.delete_at(0)
-  elsif remove == 1
-    @students.delete_at(1)
-  elsif remove == 2
-    @students.delete_at(2)
-  elsif remove == 3
-    @students.delete_at(3)
-  elsif remove == 4
-    @students.delete_at(4)
-  elsif remove == 5
-    @students.delete_at(5)
-  else
-    puts "Error. You entered is wrong command"
-  end
-  print_name
 end
