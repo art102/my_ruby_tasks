5af96322cbcd71a01c1aac0f8c6b64faf92f052d
Artur Daukaev <mckorven@gmail.com>
2017-06-12 01:24:37 +0500

    Optimization dictionary: add 'if_else'

--- dict.rb
+++ dict.rb
@@ -1,12 +1,11 @@
 # encoding: UTF-8
 
-# хеш со словами и переводами
 words = {
 	'dog' => ['собака','пёс'],
 	'cat' => ['кошка','кот'],
 	'girl' => ['девушка','девочка'],
-	'boy' => 'мальчик',
-	'boll' => 'мяч',
+	'boy' => ['мальчик'],
+	'boll' => ['мяч'],
 	'fire' => ['огонь','пожар']
 }
 
@@ -14,18 +13,14 @@ loop do
 
   print 'Введите слово: '
   input = gets.strip.downcase
- 
-  # Если тим массив, то определяем рамер и выводим количество
-  if words[input].class == Array
+
+  if input == ''
+    exit
+  elsif words.include?(input)
     puts "Переводов #{words[input].size}:"
     puts words[input]
-  elsif input == ''
-    exit
-  elsif words[input].class == String
-    puts "Перевод 1:"
-    puts words[input]
   else
-    puts 'Извините, но в словаре нет такого слова'
+    puts 'Извините, но в словаре такого слова нет.'
   end
 
 end
