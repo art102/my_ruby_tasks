03e05bc7cadef7d59171ceb45922b23eb24a5edf
Artur Daukaev <mckorven@gmail.com>
2017-06-14 21:47:14 +0500

    Initialize new small game - slot machine

---
+++ bandit.rb
@@ -0,0 +1,27 @@
+numbers = {}
+
+print 'Press Enter to start game...'
+gets
+
+money = 100
+
+print 'Would you like to play? (Y/N): '
+user_input = gets.strip.capitalize
+
+print 'How old are you?: '
+user_age = gets.strip.to_i
+
+if input == "Y" & user_age >= 18
+	puts "Ok, let's play!"
+else
+	puts 'You are still a child! Go home!'
+	exit
+end
+
+while money > 0
+
+	x = rand(100..999)
+
+  
+
+end	
