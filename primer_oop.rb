62f3d41882ecd6f8540323765d648d076ef170bc
ArturDaukaev <daukaevar@bashneft.ru>
2017-06-19 15:52:08 +0500

    Create comments

--- primer_oop.rb
+++ primer_oop.rb
@@ -1,3 +1,4 @@
+# создание класса - Альбом
 class Album
 	attr_reader :name, :songs
 
@@ -6,11 +7,13 @@ class Album
 		@songs = []
 	end	
 
+	# добавление песни в альбом
 	def add_song song
 		@songs << song
 	end
 end
 
+# Создание класса - Песня
 class Song
 	attr_reader :name, :duration
 
@@ -20,12 +23,15 @@ class Song
 	end
 end
 
+# создаём альбом
 album = Album.new 'My favourite songs'
 
+# создаём три песни
 song1 = Song.new 'Let it be', 4
 song2 = Song.new 'We are living in America!', 5
 song3 = Song.new 'Wind of change', 6
 
+# добавляем наши песни в альбом
 album.add_song song1
 album.add_song song2
 album.add_song song3
