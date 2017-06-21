53b18cacfe4e2ebdd4abf3cbce21cc348af42201
ArturDaukaev <daukaevar@bashneft.ru>
2017-06-21 16:34:32 +0500

    Add useful comments to programm

--- word_count.rb
+++ word_count.rb
@@ -1,6 +1,12 @@
+# Author Artur Daukaev
+# 21/06/2017
+
+# Asking user
 print 'Enter sentence: '
+# Write users input to variable 'input'
 input = gets.strip
 
+# create array of words
 arr_words = input.split(' ')
 
 arr_words.each_with_index do |word, index|
